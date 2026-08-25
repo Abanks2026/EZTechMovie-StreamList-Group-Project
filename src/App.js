6cad6184812c3ba554b1934dfd397c6f9b4d180e
Abanks2026 <anthony.banks3@student.uagc.edu>
2026-08-24 21:56:48 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import Login from "./Pages/Login";
 
 import Navigation from "./components/Navigation";
 import StreamList from "./Pages/StreamList";
-import Movies from "./Pages/Movies";
 import TMDBMovies from "./Pages/TMDBMovies";
 import Products from "./Pages/Products";
 import Cart from "./Pages/Cart";
+import CreditCard from "./Pages/CreditCard";
 import About from "./Pages/About";
 
 import "./App.css";
 
 function App() {
+
+    const [user, setUser] = useState(() => {
+    const savedUser =
+      localStorage.getItem("streamListUser");
+
+    return savedUser
+      ? JSON.parse(savedUser)
+      : null;
+  });
+
+
   const [cart, setCart] = useState(() => {
     const savedCart = localStorage.getItem("streamListCart");
-
     return savedCart ? JSON.parse(savedCart) : [];
   });
 
-  // warning message for dupes
+  
 
   const [cartWarning, setCartWarning] = useState("");
 
@@ -29,57 +40,62 @@ function App() {
     );
   }, [cart]);
 
-function addToCart(product) {
-  const isSubscription =
-    product.id >= 1 && product.id <= 4;
+  function handleLogout() {
+    setUser(null);
 
-  // Check whether ANY subscription is already in the cart
-  const subscriptionInCart = cart.find(
-    (item) => item.id >= 1 && item.id <= 4
+    localStorage.removeItem(
+      "streamListUser"
   );
+ }
 
-  // Stop user from adding another subscription
-  if (isSubscription && subscriptionInCart) {
-    setCartWarning(
-      `You already have the ${subscriptionInCart.service} in your cart. Only one subscription plan may be selected at a time.`
+
+  function addToCart(product) {
+    const isSubscription =
+      product.id >= 1 && product.id <= 4;
+
+    const subscriptionInCart = cart.find(
+      (item) => item.id >= 1 && item.id <= 4
     );
 
-    setTimeout(() => {
-      setCartWarning("");
-    }, 4000);
-
-    return;
-  }
-
-  setCart((currentCart) => {
-    const existingItem = currentCart.find(
-      (item) => item.id === product.id
-    );
-
-    // Merchandise can have multiple quantities
-    if (existingItem) {
-      return currentCart.map((item) =>
-        item.id === product.id
-          ? {
-              ...item,
-              quantity: item.quantity + 1,
-            }
-          : item
+    if (isSubscription && subscriptionInCart) {
+      setCartWarning(
+        `You already have the ${subscriptionInCart.service} in your cart. Only one subscription plan may be selected at a time.`
       );
+
+      setTimeout(() => {
+        setCartWarning("");
+      }, 4000);
+
+      return;
     }
 
-    // Add new item
-    return [
-      ...currentCart,
-      {
-        ...product,
-        quantity: 1,
-      },
-    ];
-  });
+    setCart((currentCart) => {
+      const existingItem = currentCart.find(
+        (item) => item.id === product.id
+      );
 
-  setCartWarning("");
-}
+      if (existingItem) {
+        return currentCart.map((item) =>
+          item.id === product.id
+            ? {
+                ...item,
+                quantity: item.quantity + 1,
+              }
+            : item
+        );
+      }
+
+      return [
+        ...currentCart,
+        {
+          ...product,
+          quantity: 1,
+        },
+      ];
+    });
+
+    setCartWarning("");
+  }
 
   function removeFromCart(id) {
     setCart((currentCart) =>
@@ -87,37 +103,41 @@ function addToCart(product) {
     );
   }
 
-function increaseQuantity(id) {
-  const item = cart.find(
-    (cartItem) => cartItem.id === id
-  );
-
-  const isSubscription =
-    item && item.id >= 1 && item.id <= 4;
-
-  if (isSubscription) {
-    setCartWarning(
-      "Subscription plans are limited to a quantity of one."
-    );
-
-    setTimeout(() => {
-      setCartWarning("");
-    }, 4000);
-
-    return;
+  function clearCart() {
+  	setCart([]);
   }
 
-  setCart((currentCart) =>
-    currentCart.map((item) =>
-      item.id === id
-        ? {
-            ...item,
-            quantity: item.quantity + 1,
-          }
-        : item
-    )
-  );
-}
+  function increaseQuantity(id) {
+    const item = cart.find(
+      (cartItem) => cartItem.id === id
+    );
+
+    const isSubscription =
+      item && item.id >= 1 && item.id <= 4;
+
+    if (isSubscription) {
+      setCartWarning(
+        "Subscription plans are limited to a quantity of one."
+      );
+
+      setTimeout(() => {
+        setCartWarning("");
+      }, 4000);
+
+      return;
+    }
+
+    setCart((currentCart) =>
+      currentCart.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          : item
+      )
+    );
+  }
 
   function decreaseQuantity(id) {
     setCart((currentCart) =>
@@ -132,52 +152,58 @@ function increaseQuantity(id) {
     );
   }
 
+/* If user is not logged in,
+   display the Google Login page */
+if (!user) {
+  return <Login setUser={setUser} />;
+}
+
   return (
-    <div className="App">
-      <Navigation cartCount={cart.length} />
-
-      <main className="page-container">
-        <Routes>
-          <Route path="/" element={<StreamList />} />
-
-          <Route
-            path="/movies"
-            element={<Movies />}
-          />
-
-          <Route
-            path="/tmdb"
-            element={<TMDBMovies />}
-          />
-
-          <Route
-            path="/products"
-            element={
-              <Products addToCart={addToCart}
-	      cartWarning={cartWarning} />
+  <div className="App">
+ <Navigation cartCount={cart.length}
+ handleLogout={handleLogout} />
+ <main className="page-container">
+  <Routes>
+    <Route path="/" element={<StreamList />} />
+    <Route
+      path="/tmdb"
+      element={<TMDBMovies />}
+    />
+    
+    <Route
+    path="/products"
+    element={
+    <Products
+    addToCart={addToCart}
+    cartWarning={cartWarning}/>
             }
           />
 
           <Route
-            path="/cart"
-            element={
-              <Cart
-                cart={cart}
-                removeFromCart={removeFromCart}
-                increaseQuantity={increaseQuantity}
-                decreaseQuantity={decreaseQuantity}
-              />
-            }
+          path="/cart"
+          element={
+          <Cart
+          cart={cart}
+          removeFromCart={removeFromCart}
+          clearCart={clearCart}
+          increaseQuantity={increaseQuantity}
+          decreaseQuantity={decreaseQuantity}
+          />
+        }
           />
 
           <Route
-            path="/about"
-            element={<About />}
+            path="/checkout"
+            element={<CreditCard />}
+          />
+          <Route
+          path="/about"
+          element={<About />}
           />
         </Routes>
-      </main>
-    </div>
-  );
-}
+        </main>
+        </div>
+        );
+      }
 
 export default App;
